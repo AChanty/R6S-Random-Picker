e81210c8b69b9678dab841d69b0aeeddc8c788c6
Anthony Chanty <anthony.chanty@hotmail.com>
2022-10-09 22:07:11 +0200

    refactor use export/import, concat allOps arrays

---
+++ js/operators.js
@@ -0,0 +1,331 @@
+export const attackers = [
+    {
+        "name": "Ace",
+        "image": "img/op-portraits/ace.webp",
+        "icon": "img/op-icons/ace.svg"
+    },
+    {
+        "name": "Amaru",
+        "image": "img/op-portraits/amaru.webp",
+        "icon": "img/op-icons/amaru.svg"
+    },
+    {
+        "name": "Ash",
+        "image": "img/op-portraits/Ash.webp",
+        "icon": "img/op-icons/Ash.svg"
+    },
+    {
+        "name": "Blackbeard",
+        "image": "img/op-portraits/Blackbeard.webp",
+        "icon": "img/op-icons/Blackbeard.svg"
+    },
+    {
+        "name": "Blitz",
+        "image": "img/op-portraits/Blitz.webp",
+        "icon": "img/op-icons/Blitz.svg"
+    },
+    {
+        "name": "Buck",
+        "image": "img/op-portraits/Buck.webp",
+        "icon": "img/op-icons/Buck.svg"
+    },
+    {
+        "name": "Capitão",
+        "image": "img/op-portraits/Capitao.webp",
+        "icon": "img/op-icons/Capitao.svg"
+    },
+    {
+        "name": "Dokkaebi",
+        "image": "img/op-portraits/Dokkaebi.webp",
+        "icon": "img/op-icons/Dokkaebi.svg"
+    },
+    {
+        "name": "Finka",
+        "image": "img/op-portraits/Finka.webp",
+        "icon": "img/op-icons/Finka.svg"
+    },
+    {
+        "name": "Flores",
+        "image": "img/op-portraits/Flores.webp",
+        "icon": "img/op-icons/Flores.svg"
+    },
+    {
+        "name": "Fuze",
+        "image": "img/op-portraits/Fuze.webp",
+        "icon": "img/op-icons/Fuze.svg"
+    },
+    {
+        "name": "Glaz",
+        "image": "img/op-portraits/Capitao.webp",
+        "icon": "img/op-icons/Capitao.svg"
+    },
+    {
+        "name": "Gridlock",
+        "image": "img/op-portraits/Gridlock.webp",
+        "icon": "img/op-icons/Gridlock.svg"
+    },
+    {
+        "name": "Grim",
+        "image": "img/op-portraits/Grim.webp",
+        "icon": "img/op-icons/Grim.svg"
+    },
+    {
+        "name": "Hibana",
+        "image": "img/op-portraits/Hibana.webp",
+        "icon": "img/op-icons/Hibana.svg"
+    },
+    {
+        "name": "Iana",
+        "image": "img/op-portraits/Iana.webp",
+        "icon": "img/op-icons/Iana.svg"
+    },
+    {
+        "name": "IQ",
+        "image": "img/op-portraits/iq.webp",
+        "icon": "img/op-icons/iq.svg"
+    },
+    {
+        "name": "Jackal",
+        "image": "img/op-portraits/Jackal.webp",
+        "icon": "img/op-icons/Jackal.svg"
+    },
+    {
+        "name": "Kali",
+        "image": "img/op-portraits/Kali.webp",
+        "icon": "img/op-icons/Kali.svg"
+    },
+    {
+        "name": "Lion",
+        "image": "img/op-portraits/Lion.webp",
+        "icon": "img/op-icons/Lion.svg"
+    },
+    {
+        "name": "Maverick",
+        "image": "img/op-portraits/Maverick.webp",
+        "icon": "img/op-icons/Maverick.svg"
+    },
+    {
+        "name": "Montagne",
+        "image": "img/op-portraits/Montagne.webp",
+        "icon": "img/op-icons/Montagne.svg"
+    },
+    {
+        "name": "Nokk",
+        "image": "img/op-portraits/Nokk.webp",
+        "icon": "img/op-icons/Nokk.svg"
+    },
+    {
+        "name": "Nomad",
+        "image": "img/op-portraits/Nomad.webp",
+        "icon": "img/op-icons/Nomad.svg"
+    },
+    {
+        "name": "Osa",
+        "image": "img/op-portraits/Osa.webp",
+        "icon": "img/op-icons/Osa.svg"
+    },
+    {
+        "name": "Sens",
+        "image": "img/op-portraits/Sens.webp",
+        "icon": "img/op-icons/Sens.svg"
+    },
+    {
+        "name": "Sledge",
+        "image": "img/op-portraits/Sledge.webp",
+        "icon": "img/op-icons/Sledge.svg"
+    },
+    {
+        "name": "Thatcher",
+        "image": "img/op-portraits/Thatcher.webp",
+        "icon": "img/op-icons/Thatcher.svg"
+    },
+    {
+        "name": "Thermite",
+        "image": "img/op-portraits/Thermite.webp",
+        "icon": "img/op-icons/Thermite.svg"
+    },
+    {
+        "name": "Twitch",
+        "image": "img/op-portraits/Twitch.webp",
+        "icon": "img/op-icons/Twitch.svg"
+    },
+    {
+        "name": "Ying",
+        "image": "img/op-portraits/Ying.webp",
+        "icon": "img/op-icons/Ying.svg"
+    },
+    {
+        "name": "Zero",
+        "image": "img/op-portraits/Zero.webp",
+        "icon": "img/op-icons/Zero.svg"
+    },
+    {
+        "name": "Zofia",
+        "image": "img/op-portraits/Zofia.webp",
+        "icon": "img/op-icons/Zofia.svg"
+    }
+];
+export const defenders = [
+    {
+        "name": "Alibi",
+        "image": "img/op-portraits/Alibi.webp",
+        "icon": "img/op-icons/Alibi.svg"
+    },
+    {
+        "name": "Aruni",
+        "image": "img/op-portraits/Aruni.webp",
+        "icon": "img/op-icons/Aruni.svg"
+    },
+    {
+        "name": "Azami",
+        "image": "img/op-portraits/Azami.webp",
+        "icon": "img/op-icons/Azami.svg"
+    },
+    {
+        "name": "Bandit",
+        "image": "img/op-portraits/Bandit.webp",
+        "icon": "img/op-icons/Bandit.svg"
+    },
+    {
+        "name": "Castle",
+        "image": "img/op-portraits/Castle.webp",
+        "icon": "img/op-icons/Castle.svg"
+    },
+    {
+        "name": "Caveira",
+        "image": "img/op-portraits/Caveira.webp",
+        "icon": "img/op-icons/Caveira.svg"
+    },
+    {
+        "name": "Clash",
+        "image": "img/op-portraits/Clash.webp",
+        "icon": "img/op-icons/Clash.svg"
+    },
+    {
+        "name": "Doc",
+        "image": "img/op-portraits/Doc.webp",
+        "icon": "img/op-icons/Doc.svg"
+    },
+    {
+        "name": "Echo",
+        "image": "img/op-portraits/Echo.webp",
+        "icon": "img/op-icons/Echo.svg"
+    },
+    {
+        "name": "Ela",
+        "image": "img/op-portraits/Ela.webp",
+        "icon": "img/op-icons/Ela.svg"
+    },
+    {
+        "name": "Frost",
+        "image": "img/op-portraits/Frost.webp",
+        "icon": "img/op-icons/Frost.svg"
+    },
+    {
+        "name": "Goyo",
+        "image": "img/op-portraits/Goyo.webp",
+        "icon": "img/op-icons/Goyo.svg"
+    },
+    {
+        "name": "Jager",
+        "image": "img/op-portraits/Jager.webp",
+        "icon": "img/op-icons/Jager.svg"
+    },
+    {
+        "name": "Kaid",
+        "image": "img/op-portraits/Kaid.webp",
+        "icon": "img/op-icons/Kaid.svg"
+    },
+    {
+        "name": "Kapkan",
+        "image": "img/op-portraits/Kapkan.webp",
+        "icon": "img/op-icons/Kapkan.svg"
+    },
+    {
+        "name": "Lesion",
+        "image": "img/op-portraits/Lesion.webp",
+        "icon": "img/op-icons/Lesion.svg"
+    },
+    {
+        "name": "Maestro",
+        "image": "img/op-portraits/Maestro.webp",
+        "icon": "img/op-icons/Maestro.svg"
+    },
+    {
+        "name": "Melusi",
+        "image": "img/op-portraits/Melusi.webp",
+        "icon": "img/op-icons/Melusi.svg"
+    },
+    {
+        "name": "Mira",
+        "image": "img/op-portraits/Mira.webp",
+        "icon": "img/op-icons/Mira.svg"
+    },
+    {
+        "name": "Mozzie",
+        "image": "img/op-portraits/Mozzie.webp",
+        "icon": "img/op-icons/Mozzie.svg"
+    },
+    {
+        "name": "Mute",
+        "image": "img/op-portraits/Mute.webp",
+        "icon": "img/op-icons/Mute.svg"
+    },
+    {
+        "name": "Oryx",
+        "image": "img/op-portraits/Oryx.webp",
+        "icon": "img/op-icons/Oryx.svg"
+    },
+    {
+        "name": "Pulse",
+        "image": "img/op-portraits/Pulse.webp",
+        "icon": "img/op-icons/Pulse.svg"
+    },
+    {
+        "name": "Rook",
+        "image": "img/op-portraits/Rook.webp",
+        "icon": "img/op-icons/Rook.svg"
+    },
+    {
+        "name": "Smoke",
+        "image": "img/op-portraits/Smoke.webp",
+        "icon": "img/op-icons/Smoke.svg"
+    },
+    {
+        "name": "Tachanka",
+        "image": "img/op-portraits/Tachanka.webp",
+        "icon": "img/op-icons/Tachanka.svg"
+    },
+    {
+        "name": "Thorn",
+        "image": "img/op-portraits/Thorn.webp",
+        "icon": "img/op-icons/Thorn.svg"
+    },
+    {
+        "name": "Thunderbird",
+        "image": "img/op-portraits/Thunderbird.webp",
+        "icon": "img/op-icons/Thunderbird.svg"
+    },
+    {
+        "name": "Valkyrie",
+        "image": "img/op-portraits/Valkyrie.webp",
+        "icon": "img/op-icons/Valkyrie.svg"
+    },
+    {
+        "name": "Vigil",
+        "image": "img/op-portraits/Vigil.webp",
+        "icon": "img/op-icons/Vigil.svg"
+    },
+    {
+        "name": "Wamai",
+        "image": "img/op-portraits/Wamai.webp",
+        "icon": "img/op-icons/Wamai.svg"
+    },
+    {
+        "name": "Warden",
+        "image": "img/op-portraits/Warden.webp",
+        "icon": "img/op-icons/Warden.svg"
+    }
+];
+
+export const allOps = attackers.concat(defenders) // merge les deux tableaux en un seul
